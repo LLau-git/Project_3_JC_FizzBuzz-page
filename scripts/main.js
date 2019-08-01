e387824f7b6cf0d221421fcf687e3003914a5be7
Laura Luca <laura.luca90@gmail.com>
2019-08-01 10:23:20 +0300

    starting main

---
+++ scripts/main.js
@@ -0,0 +1,23 @@
+const main = document.getElementById("container");
+
+const btnRef = document.getElementById("btnRefresh"); 
+
+
+//creating container of 100 div
+function fizzBuzz() {
+    console.log("Creating container");
+    const fizzBuzz = document.getElementById("container");
+    for(let i=1; i<= 100; i++) {
+        console.log(i);
+    }
+
+//     // for (var i=1; i < 101; i++){
+//     //     if (i % 15 == 0) console.log("FizzBuzz");
+//     //     else if (i % 3 == 0) console.log("Fizz");
+//     //     else if (i % 5 == 0) console.log("Buzz");
+//     //     else console.log(i);
+//     // }  
+//     const output = fizzBuzz();
+//     console.log(output);
+    
+// }
