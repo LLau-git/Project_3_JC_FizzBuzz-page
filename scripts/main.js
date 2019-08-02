582b9e8aed471010c23736d16c17a186bbd1f4d9
Laura Luca <laura.luca90@gmail.com>
2019-08-03 00:09:53 +0300

    creating fizzbuzz with for loop

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,9 @@
 const btnRef = document.getElementById("btnRefresh"); 
 // const main = document.getElementById("container");
 
+let myDiv = document.createElement("div");
+let myPar = document.createElement("p");
 
-
-// var myDiv = document.createElement("div");
-// // for(let i=1; i<= 100; i++) 
-// myDiv.innerHTML = "numbers 1";
-// document.getElementById("container").appendChild(myDiv); 
 
 
 function main() {
@@ -16,18 +13,38 @@ function main() {
     const main = document.getElementById('container');
 
 
-    for(let i=1; i<= 100; i++) {
-        const myDiv = document.createElement("div");
-        myDiv.innerText = "Number # "+i;
+    for (let i=1; i<= 100; i++) {
+        let myDiv = document.createElement("div");
+        myDiv.innerText = i;
         myDiv.id = "myid"+i;
+        myDiv.className = "myDivs";
+        myDiv.className += " numbers";
+
+        if (i % 15 == 0){
+            console.log("FizzBuzz");
+            myDiv.innerText += " FizzBuzz";
+            myDiv.className = "fizzbuzzClass"
+            } 
+        if (i % 3 == 0) {
+            console.log("Fizz");
+            myDiv.innerText += " Fizz";
+            myDiv.className = "fizzClass"
+            } 
+        if (i % 5 == 0) {
+            console.log("Buzz");
+            myDiv.innerText += " Buzz";
+            myDiv.className = "buzzClass"
+            }
+        
         // console.log(i);
         main.appendChild(myDiv);
         console.log("Create Main");
     }    
 }
+
 main();
-     
-        
+    
+
 
 // function myFunction() {
 //     document.getElementById("demo").innerHTML = "Hello World";
